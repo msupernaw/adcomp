b2cd85c302f17ea849084bc734179379b5d1bf5e
Kasper Kristensen <kaskr@imm.dtu.dk>
2014-06-18 15:51:31 +0200

    Using std::vector and algorithm::sort. Add myReverse header.

--- TMB/inst/include/cppad/local/ad_fun.hpp
+++ TMB/inst/include/cppad/local/ad_fun.hpp
@@ -1,6 +1,8 @@
 /* $Id: ad_fun.hpp 3301 2014-05-24 05:20:21Z bradbell $ */
 # ifndef CPPAD_AD_FUN_INCLUDED
 # define CPPAD_AD_FUN_INCLUDED
+#include <vector>
+#include <algorithm>
 
 /* --------------------------------------------------------------------------
 CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-14 Bradley M. Bell
@@ -289,6 +291,10 @@ public:
 	/// reverse mode sweep
 	template <typename VectorBase>
 	VectorBase Reverse(size_t p, const VectorBase &v);
+
+	/// kaspers reverse mode sweep
+	template <typename VectorBase>
+	void myReverse(size_t p, const VectorBase &v, size_t dep_var_index, VectorBase &value);
 
 	// forward mode Jacobian sparsity 
 	// (see doxygen documentation in for_sparse_jac.hpp)
