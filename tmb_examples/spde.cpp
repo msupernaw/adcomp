4a4a51463837fac6a377b224b06a32583886121c
Kasper Kristensen <kaskr@imm.dtu.dk>
2015-02-03 09:40:51 +0100

    cleanup spde.cpp

--- tmb_examples/spde.cpp
+++ tmb_examples/spde.cpp
@@ -1,3 +1,4 @@
+// Leukemia example from Lindgren et al 2011, JRSS-B
 #include <TMB.hpp>
 
 template<class Type>
@@ -17,7 +18,7 @@ Type objective_function<Type>::operator() ()
   PARAMETER(log_kappa);
   PARAMETER(log_alpha);  
   PARAMETER_VECTOR(x);  
-  
+
   Type tau = exp(log_tau);
   Type kappa = exp(log_kappa);
   Type alpha = exp(log_alpha);
@@ -26,15 +27,15 @@ Type objective_function<Type>::operator() ()
 
   SparseMatrix<Type> Q = Q_spde(spde,kappa);
 
-  nll = GMRF(Q)(x);									// Negative log likelihood
+  nll = GMRF(Q)(x);                              // Negative log likelihood
 
   vector<Type> Xbeta = X*beta;  
   for(int i=0; i<time.size(); i++){    
     Type eta = Xbeta(i) + x(meshidxloc(i))/tau;
     Type lambda = exp(eta);
     Type t_alpha = pow(time(i),alpha);
-    Type S = exp(-lambda*t_alpha);         		// Survival funciton
-    Type f = lambda*alpha*t_alpha/time(i)*S; 	// Densities
+    Type S = exp(-lambda*t_alpha);               // Survival function
+    Type f = lambda*alpha*t_alpha/time(i)*S;     // Densities
 
     // Likelihood contribution depends on truncation status
     if(notcens(i))
